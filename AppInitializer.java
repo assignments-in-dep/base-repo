c0e702410b70f73a5c068783615760040b0a7fd6
LahiruHarshana <lharshana2002@gmail.com>
2024-08-08 13:15:19 +0530

    Add AppInitializer.java

---
+++ AppInitializer.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+/**
+ * @File: AppInitializer
+ * @author : L.H.J
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-08-08, Thursday
+**/
+public class AppInitializer {
+    public static void main(String[] args) {
+        System.out.println("Let's init");
+    }
+}
